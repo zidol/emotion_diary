a4405b9edbcf9f88a6c43ed5bf36ad252bcaa019
zidol <ajh3166@naver.com>
2025-08-23 18:26:29 +0900

    Feat : 라우터 기초틀 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import { Route, Routes } from "react-router-dom";
+import Notfound from "./pages/Notfound";
 
 function App() {
-  return <>hello</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
 
 export default App;
