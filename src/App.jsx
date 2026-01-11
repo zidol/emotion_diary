2ccdf085ac4dea86e4914f3ecc2bf912cc6cbe6e
zidol <ajh3166@naver.com>
2026-01-11 22:58:46 +0900

    feat : 에디터 페이지 구현하기 UI 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,19 @@ import Notfound from "./pages/Notfound";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date("2025-11-16").getTime(),
+    createdDate: new Date("2026-01-11").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date("2025-11-15").getTime(),
+    createdDate: new Date("2026-01-10").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
   },
   {
     id: 3,
-    createdDate: new Date("2025-10-15").getTime(),
+    createdDate: new Date("2026-01-10").getTime(),
     emotionId: 3,
     content: "3번 일기 내용",
   },
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,16 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Edidtor from "../components/Editor";
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로가기"} />}
+      />
+      <Edidtor />
+    </div>
+  );
 };
 
 export default New;
